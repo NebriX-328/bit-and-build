eb258a2bd2380aeedb0072b0309a3c718955158d
NebriX <pika223344chu@gmail.com>
2025-09-13 23:51:17 +0530

    Update settings.py

--- fund-tracker-django/fund_tracker/settings.py
+++ fund-tracker-django/fund_tracker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
+}
+
+USD_TO_INR_RATE = 83.50
+
+GEMINI_API_KEY = "AIzaSyCbgAH70Cy-iuB6vTM5TZSoAhpo2X43V40"
